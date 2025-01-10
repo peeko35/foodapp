c8c436b4466c74db76a92a27e5520fc5100c4df3
gauri <peeko5438@gmail.com>
2025-01-10 22:47:43 +0530

    new changes

--- app/src/main/java/com/example/myapplication/UserHome.java
+++ app/src/main/java/com/example/myapplication/UserHome.java
@@ -1,13 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -16,6 +23,8 @@ public class UserHome extends AppCompatActivity {
     Button button;
     TextView textView;
     FirebaseUser user;
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,8 @@ public class UserHome extends AppCompatActivity {
         button=findViewById(R.id.logout);
         textView=findViewById(R.id.user_details);
         user= auth.getCurrentUser();
+        bottomNavigationView=findViewById(R.id.bottomNavView);
+        frameLayout=findViewById(R.id.framelayout);
 
         if(user==null){
             Intent intent = new Intent(getApplicationContext(),UserLogin.class);
@@ -47,5 +58,43 @@ public class UserHome extends AppCompatActivity {
             }
         });
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId=item.getItemId();
+                if(itemId==R.id.navHome){
+                    loadFragment(new HomeFragment(),false);
+
+
+                }else if(itemId==R.id.navExplore){
+                    loadFragment(new SearchFragment(),false);
+
+                }else if(itemId==R.id.navafav){
+                    loadFragment(new FavFragment(),false);
+
+                }else {// user profile
+                    loadFragment(new UserProfileFragment(),false);
+
+                }
+
+                return true;
+            }
+        });
+        loadFragment(new HomeFragment(),true);
+
+    }
+    private void loadFragment(Fragment fragment,boolean isAppInitialized){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+
+        if(isAppInitialized){
+            fragmentTransaction.add(R.id.framelayout,fragment);
+        }
+        else{
+            fragmentTransaction.replace(R.id.framelayout,fragment);
+
+        }
+
+        fragmentTransaction.commit();
     }
 }
